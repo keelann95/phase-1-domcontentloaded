3d50cf7441c96632d6b5512a4570388b9dbe0ab3
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-08 12:50:05 +0300

    modified  index.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-// // Your code goes here
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     console.log("The DOM has loaded");
-//     let newParagraph= document.querySelector('p').textContent("JavaScript is so cool. It lets me add text to my page programmatically." )
-// newParagraph = document.getElementById('text').innerHTML('This is really cool!')
-//   });
-
-// function  event() {
-    
-
-
-// const LoadEvent = document.createEvent('Event')
-// LoadEvent.initEvent('DOMContentLoaded', true, true)
-// window.document.dispatchEvent(LoadEvent)
-
-// // const newParagraph= document.querySelector('p')
-// // newParagraph=document.getElementById('text').innerContent('This is really cool!')
-// }
-// event()
 document.addEventListener('DOMContentLoaded', () =>{
   
 document.querySelector('p').textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
